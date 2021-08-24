28e8e592f58a37729b2d099221a8118600f4b8c8
Baha-Abdelatif <baha.abdelatif@gmail.com>
2021-08-24 16:32:18 +0200

    Set up Strapi Backend & React Frontend

---
+++ srv/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0ca77f55cd6ba302c627c75e5752a245'),
+    },
+  },
+});
---
+++ srv/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '806df938-8a14-48a8-ba40-f617d8708743'
+};
